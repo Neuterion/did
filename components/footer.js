c53d3c9ccacf957dbbef0f9ec9b487e061dbcea3
Neuterion <farzanakhtarf99@gmail.com>
2022-03-27 21:11:45 +0700

    removed unnecessary content on index

--- components/footer.js
+++ components/footer.js
@@ -7,16 +7,16 @@ export default function Footer() {
     return (
       <div className="flex-[0_1_auto] w-11/12 mx-auto break-all xs:break-normal">
         <footer className={`flex flex-col md:flex-row py-3 space-y-2 md:space-y-0 justify-center items-center font-[Inter] text-white border-t border-opacity-50 border-t-gray-100`}>
-          <h3 className={`mx-2 text-xs xxs:text-sm xs:text-base`}>
+          <h3 className={`mx-2 text-xs xxs:text-sm sm:text-base`}>
             Hubungi kami:
           </h3>
           <a href="https://www.instagram.com/dcxfg115/" target="_blank" rel="noreferrer" className={`py-2 px-3 mx-2 flex justify-center items-center rounded-lg bg-gray-900 hover:bg-black transition duration-150`}>
-            <Image src={instagram} width="30px" height="30px" alt="Instagram"></Image>
-            <h3 className={`px-2 text-xs xxs:text-sm xs:text-base`}>@dcxfg115</h3>
+            <Image src={instagram} width="20%" height="20%" alt="Instagram"></Image>
+            <h3 className={`px-2 text-xs xxs:text-sm sm:text-base`}>@dcxfg115</h3>
           </a>
           <a className={`py-2 px-3 mx-2 flex justify-center items-center rounded-lg bg-gray-900 hover:bg-black transition duration-150`}>
-            <Image src={gmail} width="35%" height="30%" alt="G-Mail"></Image>
-            <h3 className={`px-2 text-xs xxs:text-sm xs:text-base`}>
+            <Image src={gmail} width="20%" height="15%" alt="G-Mail"></Image>
+            <h3 className={`px-2 text-xs xxs:text-sm sm:text-base`}>
               drawingclubjkt115<br className="hidden xsl:block sm:hidden"/>@gmail.com
             </h3>
           </a>
--- pages/index.js
+++ pages/index.js
@@ -12,26 +12,12 @@ export default function Home() {
 function Main() {
   return (
     <main className={`flex-[1_1_auto] flex flex-col font-[Jost,sans-serif] items-center text-white break-all`}>
-      <div className={`flex justify-center w-1/2 h-1/2 object-cover py-4`}>
+      <div className={`flex justify-center w-1/2 h-1/2 lg:w-[33%] lg:h-[33%] object-cover py-4`}>
         <Image src={title} alt="Dreaming in Daylight" placeholder="blur"/>
       </div>
-      <h4 className={`text-xs md:text-base 2xl:text-lg xs:break-normal pb-4 px-8 sm:px-32 2xl:px-96 text-center`}>
-        Sebuah acara yang mengadakan lomba dan pameran seni di bidang menggambar, dan fotografi;
-        bertema <span className="italic">daydreaming</span> yang diasosiasikan dengan kemampuan berimajinasi seorang diri dalam berfoto maupun menggambar.{" "}
-        <span className="italic">Daydreaming in Daylight</span> dapat diikuti oleh seluruh siswa/i SMP di Jakarta Selatan.
+      <h4 className={`text-md xs:break-normal px-8 sm:px-32 2xl:px-96 text-center`}>
+        Dimana kemampuan menggambar maupun fotografi menyatu dengan kemampuan berimajinasi.
       </h4>
-      <div className={`py-2 text-center`}>
-        <Drawings/>
-      </div>
-      <div className="mb-3 -mt-2 sm:-mt-1">
-        <button className="px-3 py-2 rounded-lg bg-opacity-25 bg-cyan-200 hover:bg-opacity-40 transition duration-100">
-          <Link href="/koleksi">
-            <a className="2xl:text-lg">
-              Lihat koleksi
-            </a>
-          </Link>
-        </button>
-      </div>
 
       <EventCards/>
 
@@ -42,7 +28,7 @@ function Main() {
 function EventCards() {
   return (
     <section className="flex flex-col justify-center items-center xs:break-normal">
-      <div className={`w-11/12 rounded-xl px-7 py-6 my-8 bg-gradient-to-tr from-[#0c1d58] to-[#6e4c68]`}>
+      <div className={`w-11/12 rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#0c1d58] to-[#6e4c68]`}>
         <div className="xxs:border-r-2 pr-4 border-white">
           <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl text-yellow-200`}>
             LOMBA
@@ -56,7 +42,7 @@ function EventCards() {
         </div>
       </div>
 
-      <div className={`w-11/12 rounded-xl px-7 py-6 my-8 bg-gradient-to-tr from-[#2cb8b3] to-[#cc2dfd]`}>
+      <div className={`w-11/12 rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#2cb8b3] to-[#cc2dfd]`}>
         <div className="xxs:border-r-2 pr-4 border-white">
           <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl text-yellow-200`}>
             LOMBA
@@ -70,7 +56,7 @@ function EventCards() {
         </div>
       </div>
 
-      <div className={`w-11/12 rounded-xl px-7 py-6 my-8 bg-gradient-to-tr from-[#946466] to-[#f3d56c]`}>
+      <div className={`w-11/12 rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#946466] to-[#f3d56c]`}>
         <div className="xxs:border-r-2 pr-4 border-white">
           <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl text-yellow-200`}>
             PAMERAN
