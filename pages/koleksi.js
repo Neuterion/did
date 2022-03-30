54746ecc00331ec91a0db5d6f98124659d69f125
Neuterion <farzanakhtarf99@gmail.com>
2022-03-30 08:32:28 +0700

    fixed animation repeating (no link comp.)

--- pages/koleksi.js
+++ pages/koleksi.js
@@ -1,3 +1,5 @@
+import { useInView } from 'react-intersection-observer'
+
 import Image from 'next/image'
 
 import coll1 from '../public/drawings/coll1.jpg'
@@ -15,47 +17,65 @@ import coll12 from '../public/drawings/coll12.jpg'
 import coll13 from '../public/drawings/coll13.jpg'
 
 export default function Collection() {
+    const { ref: title, inView: titleInView, entry: titleEntry } = useInView({
+      triggerOnce: true,
+    })
+    const { ref: desc, inView: descInView, entry: descEntry } = useInView({
+        triggerOnce: true,
+    })
+
     return (
-        <div id="koleksi" className={`block columns-1 xsl:columns-2 sm:columns-3 lg:columns-5 max-w-[100vw] h-max m-4`}>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll1} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+        <section>
+            <div className="m-4 sm:m-0 sm:mt-5 text-white text-center">
+                <h1 ref={title} className={`header_slide_in2 ${titleInView ? "appear" : ""} font-[Fraunces] text-3xl sm:text-4xl`}>
+                    Koleksi Kami
+                </h1>
+                <h4 ref={desc} className={`header_slide_in2 ${descInView ? "appear" : ""} font-[Inter] text-xs sm:text-sm`}>
+                    Berisi gambar-gambar hasil imajinasi anggota-anggota Drawing Club di SMPN 115.
+                </h4>
             </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll2} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll3} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll4} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll5} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll6} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll7} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll8} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll9} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll10} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll11} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll12} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-            <div className="h-auto mb-4 break-inside-avoid-column">
-                <Image src={coll13} placeholder="blur" alt="image" quality="100" layout="responsive"/>
-            </div>
-        </div>
+            <div id="koleksi" className={`block columns-1 xsl:columns-2 sm:columns-3 lg:columns-5 max-w-[100vw] h-max m-4`}>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll1} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll2} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll3} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll4} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll5} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll6} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll7} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll8} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll9} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll10} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll11} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll12} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+                <div className="h-auto mb-4 break-inside-avoid-column">
+                    <Image src={coll13} placeholder="blur" alt="image" quality="100" layout="responsive"/>
+                </div>
+            </div>            
+        </section>
+
     )
 }
