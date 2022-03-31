b4a4003ea49b77e58dde8307a0adddb58577e3da
Neuterion <farzanakhtarf99@gmail.com>
2022-03-31 11:45:37 +0700

    increased event card's content accessibility

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,18 @@ export default function Home() {
   })
 
   return (
-    <main className="flex-[1_1_auto] font-[Jost,sans-serif] items-center text-white break-all">
-      <div ref={ref} className={`flex flex-col items-center header_slide_in ${inView ? "appear" : ""}`}>
-        <div className="w-1/2 h-1/2 object-cover py-2">
-          <Image src={title} alt="Dreaming in Daylight" placeholder="blur"/>
+    <main className="flex-[1_1_auto] flex flex-col justify-center text-white break-all">
+      <div className="flex flex-col items-center justify-center">
+        <div ref={ref} className={`flex flex-col items-center header_slide_in ${inView ? "appear" : ""}`}>
+          <div className="flex flex-col items-center w-1/2 h-1/2 object-cover py-2">
+            <Image src={title} alt="Dreaming in Daylight" placeholder="blur"/>
+          </div>
+          <h4 className="w-[100%] max-w-[90ch] font-[Inter] text-xs xs:text-base xs:break-normal text-center">
+            Dimana kemampuan menggambar maupun fotografi menyatu dengan kemampuan berimajinasi.
+          </h4>        
         </div>
-        <h4 className="w-[100%] max-w-[90ch] font-[Inter] text-xs xs:text-base xs:break-normal text-center">
-          Dimana kemampuan menggambar maupun fotografi menyatu dengan kemampuan berimajinasi.
-        </h4>        
+        <EventCards/>
       </div>
-
-      <EventCards/>
-
     </main>
   )
 }
@@ -41,47 +41,62 @@ function EventCards() {
   })
 
   return (
-    <section id="index" className="flex flex-col justify-center items-center xs:break-normal">
-      <div ref={Drawry} className={`did_slide_in ${DrawryInView ? "appear" : ""} w-11/12 rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#0c1d58] to-[#6e4c68]`}>
-        <div className="xxs:border-r-2 pr-4 lg:pr-6 border-white transition-[border] duration-[90ms] ease-linear hover:border-r-8">
-          <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl text-yellow-200`}>
-            LOMBA
-          </h3>
-          <h1 className={`font-[Fraunces,serif] text-4xl md:text-7xl font-bold text-[#EFEFEF]`}>
-            Draw From A Story
-          </h1>
-          <p className={`inline-block py-1 px-2 mt-1.5 font-[Jost,sans-serif] text-xs md:text-sm bg-[#91C7C9] bg-opacity-50 rounded-lg`}>
-            Gambar apa yang kamu imajinasikan saat membaca buku, sastra atau mendengarkan lagu!
-          </p>
-        </div>
+    <section id="cards" className="flex flex-col justify-center items-center xs:break-normal text-white">
+      <div ref={Drawry} className={`flex items-center justify-end did_slide_in ${DrawryInView ? "appear" : ""} w-full rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#42275a] to-[#734b6d]`}>
+          <div className="flex-[1_1_auto] flex flex-col justify-center">
+            <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl`}>
+              LOMBA
+            </h3>
+            <h1 className={`font-[Fraunces,serif] text-4xl md:text-7xl font-bold`}>
+              Draw From A Story
+            </h1>
+            <p className={`max-w-[95ch] py-1 px-2 mt-1.5 font-[Jost,sans-serif] text-xs md:text-base rounded-lg`}>
+              Gambar apa yang kamu imajinasikan saat membaca buku, sastra atau mendengarkan lagu!
+            </p>            
+          </div>
+          <a href="">
+            <svg role="button" xmlns="http://www.w3.org/2000/svg" className="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>            
+          </a>
       </div>
 
-      <div ref={Photo} className={`did_slide_in ${PhotoInView ? "appear" : ""} w-11/12 rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#2cb8b3] to-[#cc2dfd]`}>
-        <div className="xxs:border-r-2 pr-4 lg:pr-6 border-white transition-[border] duration-[90ms] ease-linear hover:border-r-8">
-          <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl text-yellow-200`}>
-            LOMBA
-          </h3>
-          <h1 className={`font-[Fraunces,serif] text-4xl md:text-7xl font-bold text-[#EFEFEF]`}>
-            Photogyart
-          </h1>
-          <p className={`inline-block py-1 px-2 mt-1.5 font-[Jost,sans-serif] text-xs md:text-sm bg-[#91C7C9] bg-opacity-50 rounded-lg`}>
-            Ambil sebuah foto lalu deskripsikan apa yang kamu imajinasikan dalam (mengambil/melihat) foto tersebut!
-          </p>
-        </div>
+      <div ref={Photo} className={`flex items-center justify-end did_slide_in ${PhotoInView ? "appear" : ""} w-full rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#61045f] to-[#aa076b]`}>
+          <div className="flex-[1_1_auto] flex flex-col justify-center">
+            <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl`}>
+              LOMBA
+            </h3>
+            <h1 className={`font-[Fraunces,serif] text-4xl md:text-7xl font-bold`}>
+              Photogyart
+            </h1>
+            <p className={`max-w-[95ch] py-1 px-2 mt-1.5 font-[Jost,sans-serif] text-xs md:text-base rounded-lg`}>
+              Ambil sebuah foto lalu deskripsikan apa yang kamu imajinasikan dalam (mengambil/melihat) foto tersebut!
+            </p>            
+          </div>
+          <a href="">
+            <svg role="button" xmlns="http://www.w3.org/2000/svg" className="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>            
+          </a>
       </div>
 
-      <div ref={Exhibition} className={`did_slide_in ${ExhibitionInView ? "appear" : ""} w-11/12 rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#946466] to-[#f3d56c]`}>
-        <div className="xxs:border-r-2 pr-4 lg:pr-6 border-white transition-[border] duration-[90ms] ease-linear hover:border-r-8">
-          <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl text-yellow-200`}>
-            PAMERAN
-          </h3>
-          <h1 className={`font-[Fraunces,serif] text-4xl md:text-7xl font-bold text-[#EFEFEF]`}>
-            Dissolved Exhibition
-          </h1>
-          <p className={`inline-block py-1 px-2 mt-1.5 font-[Jost,sans-serif] text-xs md:text-sm bg-[#91C7C9] bg-opacity-50 rounded-lg`}>
-            Pameran yang berisikan karya-karya peserta lomba, serta siswa/i SMP di Jakarta Selatan yang ingin menampilkan gambar/foto karyanya.
-          </p>
-        </div>
+      <div ref={Exhibition} className={`flex items-center justify-end did_slide_in ${ExhibitionInView ? "appear" : ""} rounded-xl px-7 py-6 my-4 sm:my-6 bg-gradient-to-tr from-[#a40606] to-[#A7631B]`}>
+          <div className="flex-[1_1_auto] flex flex-col justify-center">
+            <h3 className={`font-[Jost,sans-serif] text-lg md:text-xl`}>
+              PAMERAN
+            </h3>
+            <h1 className={`font-[Fraunces,serif] text-4xl md:text-7xl font-bold`}>
+              Dissolved Exhibition
+            </h1>
+            <p className={`max-w-[95ch] py-1 px-2 mt-1.5 font-[Jost,sans-serif] text-xs md:text-base rounded-lg`}>
+              Pameran yang berisikan karya-karya peserta lomba, serta siswa/i SMP di Jakarta Selatan yang ingin menampilkan lukisan maupun foto karyanya.
+            </p>            
+          </div>
+          <a href="">
+            <svg role="button" xmlns="http://www.w3.org/2000/svg" className="h-11 w-11" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>            
+          </a>
       </div>
     </section>
   )
