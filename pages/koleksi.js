879777f366858323459d2222369b1fdc3f8caab9
Neuterion <farzanakhtarf99@gmail.com>
2022-04-16 20:16:12 +0700

    added lazy load blur image

--- pages/koleksi.js
+++ pages/koleksi.js
@@ -1,5 +1,8 @@
+import React from 'react'
 import { prisma } from '../lib/prisma'
 import Link from 'next/link'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 export default function Collection({ posts }) {
   return (
@@ -17,7 +20,7 @@ export default function Collection({ posts }) {
           return (
             <div key={post.id.toString()} className="h-auto mb-4 break-inside-avoid-column">
               <Link href={`/karya/${encodeURIComponent(post.id)}`} passHref>
-                <img src={post.photo} alt={post.title}/>
+                <LazyLoadImage src={post.photo} alt={post.title} effect="blur" className="cursor-grab" />
               </Link>
             </div>
           )
